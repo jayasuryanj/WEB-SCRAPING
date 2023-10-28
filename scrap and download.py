074bebe5d4a759136b9b3111f88d9a2c5d6093b7
JAYASURYAN J <102530365+jayasuryanj@users.noreply.github.com>
2023-10-28 09:57:01 +0530

    SCRAP WEB.PY

--- scrap and download.py
+++ scrap and download.py
@@ -1,48 +1,50 @@
 from selenium import webdriver
 import requests as rq
 import os
-from bs4 import beatifulsoap
+from bs4 import BeautifulSoup
 import time
 
-path = input("enter path : ")
-url = input("enter url: ")
+path = input("Enter path: ")
+url = input("Enter URL: ")
 output = "output"
+
 def get_url(path, url):
-	driver = webdriver.chrome(executable_path=r"{}".format(path))
-	driver.get(url)
-	print("loading. . . ..")
-	res driver.execute_script("return document.document.Element.outerHTML")
+    driver = webdriver.Chrome(executable_path=path)
+    driver.get(url)
+    print("Loading...")
+    time.sleep(5)  # Adjust the sleep duration as needed, ensuring the page loads before getting HTML
+    res = driver.execute_script("return document.documentElement.outerHTML")
+    driver.quit()  # Close the webdriver
 
-	return res
+    return res
 
-	def get_img_links(res):
-		soup = beatifulsoap(res, "lxml")
-		imglinks = soup.find_all("img", src=True)
-		return getlinks
+def get_img_links(res):
+    soup = BeautifulSoup(res, "html.parser")
+    img_links = soup.find_all("img", src=True)
+    return img_links
+
 def download_img(img_link, index):
-	try:
-		extensions = [".jpeg", ".png", ".gif"]
-		extensions = ".jpg"
-		for exe in extensions:
-			if img_link.find(exe > 0:
-				extension = exe
-				break 
-		img_data = rq.get(img_link.content
-		with open(output + "\\" + str(index + 1) + extension, "web+") as f:
-			f.write(img_data)
-		f.close()
-	except Exception:
-		pass
+    try:
+        extensions = [".jpeg", ".png", ".gif", ".jpg"]  # List of image extensions
+        extension = ""
+        for exe in extensions:
+            if img_link["src"].endswith(exe):
+                extension = exe
+                break
+        if extension:
+            img_data = rq.get(img_link["src"]).content
+            with open(os.path.join(output, f"{index + 1}{extension}"), "wb") as f:
+                f.write(img_data)
+    except Exception as e:
+        print(f"Failed to download image {index + 1}: {e}")
 
 result = get_url(path, url)
-time.sleep(60)
 img_links = get_img_links(result)
-if not os.path.isdir(output);
+if not os.path.isdir(output):
     os.mkdir(output)
-for index, img_link in enumerate(img_links):
-	img_link = img_link["src"]
-	print("downloading...")
-	if img_link:
-		download_img(img_link, index)
-print("download complete!")
 
+for index, img_link in enumerate(img_links):
+    print(f"Downloading image {index + 1}...")
+    download_img(img_link, index)
+
+print("Download complete!")
